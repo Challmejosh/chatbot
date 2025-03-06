c46bbf3c465a481f0625b1fd26b95b514426f53e
Challmejosh <challmejosh@gmail.com>
2025-03-06 11:01:07 -0800

    another commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,26 @@
-// import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Context from "./components/Context";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Chatgem AI",
   description: "Generate your text with AI to easily get your work done in minutes",
 };
-// ${geistSans.variable} ${geistMono.variable}
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`  antialiased`}
+        className={` ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Context>
         {children}
